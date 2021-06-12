439f5d86e4d51cb2161d3f0daf9eece0949f502e
Muhammad Sameer <muhammadsameer2016@yahoo.com>
2021-06-12 16:06:42 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -14,8 +14,25 @@ let toggle = document.querySelector('.nav-toggle');
 console.log(toggle)
 
 
+let text = document.querySelector('.centered');
+console.log(text);
+
+
 toggle.addEventListener("click" , ()=>{
     links.classList.toggle('show-links');
+    if (links.classList.contains('show-links')) {
+        // text.style.color = "blue";
+        // text.style.position = "absolute"
+        text.style.top = "315px"
+        text.style.left = "50px"
+    }
+    else{
+        // text.style.position = "absolute"
+        text.style.top = "150px"
+        text.style.left = "50px"
+        // text.style.color = "red";
+    }
+
 
     // alternative of the toggle
     // if(links.classList.contains('show-links')){
